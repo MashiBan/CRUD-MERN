96c40a529e236872287de5c8222674ce3606f6f3
MashiBan <kb00664422@gmail.com>
2024-08-28 14:16:21 +0530

    resolving

---
+++ client-side/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import Posts from "../Components/Posts";
+
+export default function HomePage() {
+    return(
+        <>
+            <Posts/>
+            <Posts/>
+            <Posts/>
+        </>
+    )
+}
---
+++ client-side/src/pages/RegisterPage.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function RegisterPage(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function register(e){
+        e.preventDefault();
+        await fetch('http://localhost:4000/register', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type' : 'application/json'},
+        });
+        if(Response.ok === 200){
+            alert('registration successful')
+        }else{
+            alert('registration failed')
+        }
+    }
+    return(
+        <div className="form">
+            <p>Welcome</p>
+            <form action="" className="register" onSubmit={register}>
+                <h2>Register</h2>
+                <input 
+                    type="text" 
+                    placeholder="username" 
+                    value={username} 
+                    onChange={e => setUsername(e.target.value)}
+                    ></input>
+                <input 
+                    type="password" 
+                    placeholder="password"
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)}
+                ></input>
+                <button>Register</button>
+            </form>
+        </div>
+    )
+}
